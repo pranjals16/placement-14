9eabdd5e7158bf098280dfb25629c13e00916916
pranjals16 <pranjals16@gmail.com>
2015-06-06 12:20:33 +0530

    Final

---
+++ worksapp/exam2/src/print.java
@@ -0,0 +1,24 @@
+
+public class print {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ImmutableQueue<Integer> data = new ImmutableQueue<Integer>();
+		ImmutableQueue<Integer> reverse = new ImmutableQueue<Integer>();
+		int num =0;
+		int max = 1000000;
+		long current = System.currentTimeMillis();
+		while(++num <= max){
+		int value = (int)(Math.random()*100);
+		data = data.enqueue(value);
+		}
+		System.out.println("Time for enqueue "+max+" randome integer "+(System.currentTimeMillis()-current)+" miliseconds");
+		while(--num>0){
+		data =data.dequeue();
+		}
+		System.out.println("Time for dequeue "+max+" randome integer "+(System.currentTimeMillis()-current)+" miliseconds");
+	}
+
+}
